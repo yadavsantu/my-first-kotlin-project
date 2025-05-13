db3638413d8059059b33dde029bd20603a3b5286
santu yadav <115726207+yadavsantu@users.noreply.github.com>
2025-05-13 21:30:58 +0545

    Basic data types in kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    // Numeric types
+    val byteVal: Byte = 127
+    val shortVal: Short = 30000
+    val intVal: Int = 123456
+    val longVal: Long = 1234567890L
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val floatVal: Float = 3.14f
+    val doubleVal: Double = 3.1415926535
+
+    // Character type
+    val charVal: Char = 'K'
+
+    // Boolean type
+    val isKotlinFun: Boolean = true
+
+    // String type
+    val message: String = "Hello, Kotlin!"
+
+    // Output all values
+    println("Byte value: $byteVal")
+    println("Short value: $shortVal")
+    println("Int value: $intVal")
+    println("Long value: $longVal")
+
+    println("Float value: $floatVal")
+    println("Double value: $doubleVal")
+
+    println("Char value: $charVal")
+    println("Boolean value: $isKotlinFun")
+    println("String value: $message")
+
+    // Accessing characters in a string
+    println("First letter of message: ${message[0]}")
 }
